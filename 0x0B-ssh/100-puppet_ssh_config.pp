7c4a2f821ac030e1dc9ad0cb66e982a4630b5962
bazileros <zalisilesithule@gmail.com>
2023-11-26 23:46:47 +0200

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 #configure ssh configure
 
 file_line{'Turn off passwd auth':
-path => '/etc/ssh/sshd_config',
+path => '/etc/ssh/ssh_config',
 line => 'PasswordAuthentication no',
 }
 
 file_line{'Declare identify file':
-path => '/etc/ssh/sshd_config',
+path => '/etc/ssh/ssh_config',
 line => 'IdentifyFile ~/.ssh/school',
 }
